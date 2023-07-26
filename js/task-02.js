a01981528b5963e21b5d31f9fa1f4e999c0196e2
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-07-26 21:17:46 +0200

    docs: update js code

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listOfIngredients = document.querySelector('#ingredients');
+
+const elementsOfIngredients = ingredients.map(element => {
+  const items = document.createElement('li');
+  items.textContent = element;
+  items.classList.add('item');
+  return items;
+});
+
+const placeItems = listOfIngredients.append(...elementsOfIngredients);
