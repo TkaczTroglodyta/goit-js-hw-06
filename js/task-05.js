01aadefbeb75bb607e3965e4329a73d4115b8157
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-07-30 13:05:38 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,14 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener('input', () => {
+  // classic if...else
+
   //   if (input.value) {
   //     output.textContent = input.value;
   //   } else {
   //     output.textContent = 'Anonymous';
   //   }
+
+  // with use of ternary operator
   output.textContent = input.value ? input.value : 'Anonymous';
 });
