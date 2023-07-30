352dc4f59d1faae218e3deb262f4881fda9cd2e0
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-07-30 11:05:51 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handleLogin);
 
+function handleLogin(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === '' || password.value === '') {
+    return console.log('Please fill in all the fields!');
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
