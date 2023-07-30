16e6523291ac74ddfe0819d09a3711d20828bc57
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-07-30 13:49:43 +0200

    Update task-04.js
    Added styles for <span id="value">. For better look :)

--- js/task-04.js
+++ js/task-04.js
@@ -30,6 +30,7 @@ const refs = {
   value: counter.querySelector('#value'),
   decrementBtn: counter.querySelector('[data-action="decrement"]'),
   incrementBtn: counter.querySelector('[data-action="increment"]'),
+  spans: counter.querySelectorAll('span'),
 };
 
 refs.decrementBtn.addEventListener('click', () => {
@@ -40,4 +41,10 @@ refs.decrementBtn.addEventListener('click', () => {
 refs.incrementBtn.addEventListener('click', () => {
   counterValue += 1;
   refs.value.textContent = counterValue;
+});
+
+refs.spans.forEach(span => {
+  span.style.width = '40px';
+  span.style.display = 'inline-block';
+  span.style.textAlign = 'center';
 });
