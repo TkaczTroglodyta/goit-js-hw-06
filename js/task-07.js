c68f767008dbc3d356e65f61b0a358a37f4f691c
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-06 19:09:07 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,9 @@
 const sizeControl = document.querySelector('#font-size-control');
 const caption = document.querySelector('#text');
 
+const initialFontSize = (Number(sizeControl.min) + Number(sizeControl.max)) / 2;
+caption.style.fontSize = `${initialFontSize}px`;
+
 sizeControl.addEventListener('input', () => {
   caption.style.fontSize = `${sizeControl.value}px`;
 });
